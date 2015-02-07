15116d3e081063f9fedb90a5ebcb2bd311c3aa6b
Daniel Wiegand <d.wiegand@firstsquare.de>
2015-02-07 16:57:04 +0100

    - basic structure

---
+++ Resources/Private/Scss/config.rb
@@ -0,0 +1,9 @@
+# https://github.com/chriseppstein/sass-css-importer
+require "sass-css-importer"
+
+http_path = "Resources/"
+css_dir = "../../Public/Css/"
+sass_dir = "./"
+images_dir = "../../Public/Image/"
+relative_assets = true
+#output_style = :compressed
